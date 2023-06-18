e1916999b22f0b40989b5c1da598f012a53f8069
sowetowp <ayodejiamzat@gmil.com>
2023-06-18 11:53:12 +0100

    any

--- utilities/generate_token.js
+++ utilities/generate_token.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 const generatetoken = (id) => {
-    return jwt.sign({id} , process.env.JWT_SECRET, {
+    return jwt.sign({id} , `${process.env.JWT_SECRET}`, {
         expiresIn: '1d'
     })
 }
